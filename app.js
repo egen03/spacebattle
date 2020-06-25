a3216abb13ccdeb210619ba9271568d13cd284cd
Student_GA111 <egen03@gmail.com>
2020-06-25 15:58:00 -0400

    added files

---
+++ app.js
@@ -0,0 +1,15 @@
+alert("Welcome to Space Battle")
+
+alert("You are the captain of the USS SChwarzenegger, on a mission to destroy all SIX alien ships. ")
+
+prompt("Choose your level (beginner or advanced.")
+
+alert("ATTACK the first alien ship.")
+
+alert("Oh no the ship has survived.")
+
+prompt("Will you attack or retreat? (Note*- If you retreat you lose- Game Over!)")
+
+alert("CONGRATS you have succesfully defeated the enemy!")
+
+
