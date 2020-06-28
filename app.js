0be79686742434bf6663cc5ba0692b48edf38c24
Student_GA111 <egen03@gmail.com>
2020-06-27 23:44:45 -0400

    Initalized objects and added attack function

--- app.js
+++ app.js
@@ -1,36 +1,102 @@
-alert("Welcome to Space Battle")
+// Make the USS_Schwarzenegger an object
+// Make a single alien ship object
+// Make a method for the USS_Schwarzenegger to attack
+//  - the target can be an input to the method
+//  - run the method and pass it to the alien ship.
+//  - the method reduces the targets hull by the firepower of the USS_Schwarzenegger
+// Make a game object
+// Make a method in the game object that will run a 'check win' for the health of the aliens +/ USS_Schwartzenegger
+// Make a method for the alien to attack
+// Make it so the attacks keep occuring until someone's hull is at 0. (repeat)
+// Make the rest of the alien ships (slightly different)with class
+// Make a loop that calls the class and generates the alien ships
+// Try out the game w/ the first ship in the array
+// Run battle w/ all ships in turn.
+// Move functions into the game object.
 
-alert("You are the captain of the USS SChwarzenegger, on a mission to destroy all SIX alien ships. ")
+// --------------------------------------------------
+// let ussShip;
+// let alienShip;
 
-prompt("Choose your level (beginner or advanced.")
+// function game() {
+//     checkWin ();
+//     ussShip = new schwarz();
+//     alienShip = new 
+// }
 
+// function attack () {
+//     fight()
+// }
 
-let result = alert('ATTACK the first alien ship!')
+// 1. Make USS_Schwarzenegger an object
 
-console.log (result)
-
-let ussShip; //declared in global scope
-
-function ussShip (hull, firepower, accuracy) {
-
-this.hull = hull;
-this.firepower = firepower;
-this.accuracy = accuracy;
-};
-
-let alienShip // declared in global scope
-function alienShip (hull, firepower, accuracy) {
-
-this.hull = hull;
-this.firepower = firepower;
-this.accuracy = accuracy;
-};
-
-let start = {
-    // name: function () {
-    //     ussShip = new ussShip();
-    //     }
+class UssShip {
+    constructor (hull, firepower, accuracy) {
+this.hull = 20,
+this.firepower = 5,
+this.accuracy = .7;
     }
+// 3. Make a method for USS_Schwarzenegger to attack
+ attack () {
+    console.log(alert('You just attacked the enemy!'))
+ }
+
+}
+
+const schwarz = new ussShip
+
+console.log(schwarz)
+
+// 2. Make alien ship an object
+
+class AlienShip {
+    constructor (hull, firepower, accuracy) {
+this.hull = Math.floor(Math.random() * 4) + 3;
+this.firepower = Math.floor(Math.random() * 3) + 2;
+this.accuracy = (Math.floor(Math.random() * 3) + 6)/ 10;
+    }
+}
+
+const enemy = new alienShip
+
+console.log(enemy)
+
+
+
+// if (hull <= 0) {
+//     console.log(alert(' The alien ship has been destroyed!'))
+// } else {
+//     console.log(alert("CONGRATS you have succesfully defeated the enemy ship!"))
+// }
+// // let defeat = math.floor(Math.random() * Math.floor(10));
+// // let result = damage + defeat;
+// }
+
+// let start = {
+//     // name: function () {
+//     //     ussShip = new ussShip();
+//     //     }
+//     }
+
+// let accuracy = Math.floor(Math.random() * Math.floor(3));
+// console.log(accuracy);
+//     if alienShip = 
+
+// let attack = function() {
+//     let damage;
+//     if (accuracy > 1) {
+//         damage = hull * firepower;
+//     } else {
+//         console.log(alert("CONGRATS you have succesfully defeated the enemy ship!"))
+//     }
+//     // let defeat = math.floor(Math.random() * Math.floor(10));
+//     // let result = damage + defeat;
+// }
+
+
+
+
+
 
 // const alienShip = {
 //   attack () { 
@@ -53,6 +119,17 @@ let start = {
 //     state = {}
 //     showinstructions
 // }
+
+// alert("Welcome to Space Battle")
+
+// alert("You are the captain of the USS SChwarzenegger, on a mission to destroy all SIX alien ships. ")
+
+// prompt("Choose your level (beginner or advanced.")
+
+
+// let result = alert('ATTACK the first alien ship!')
+
+// console.log (result)
 
 // alert("ATTACK the first alien ship.")
 
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+class alienShip {
+//     constructor (hull, firepower, accuracy) {
+// this.hull = Math.floor(Math.random() * 4) + 3;
+// this.firepower = Math.floor(Math.random() * 3) + 2;
+// this.accuracy = (Math.floor(Math.random() * 3) + 6)/ 10;
+//     }
+// attack: function () {
+//     return this.hull - this.firepower;
+// }
+
+// };
